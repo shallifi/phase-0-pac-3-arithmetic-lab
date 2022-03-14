29fb8d76baad86f2074610e9fca7bad3293c28eb
Benjamin <shallifi@hotmail.com>
2022-03-14 00:19:21 -0400

    almost Completed Assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b) {
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+function subtract(a,b) {
+    return a - b;
+}
+
+console.log(subtract(10, 2));
+
+function multiply(a,b) {
+    return a * b;
+}
+
+console.log(multiply(5, 5));
+
+function divide(a,b) {
+    return a / b;
+}
+
+console.log(divide(25, 5));
+
+/*describe('makeInt(n)', function() {
+    it('parses n as an integer and returns the parsed integer', function() {
+      expect(makeInt(a.toString())).toEqual(a)
+    })*/
+function makeInt(string){
+    
+}
+
+console.log(parseInt("n"));
+
+n = Number.parseInt("249");
+console.log (n)
